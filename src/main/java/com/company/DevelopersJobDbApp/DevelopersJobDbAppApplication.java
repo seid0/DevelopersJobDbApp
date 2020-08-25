0ff36dd9034ed23055e97bc69b5b43fe71420693
Seid Aliyev <seidaliyev98@gmail.com>
2020-08-25 23:20:33 +0400

    changes

---
+++ src/main/java/com/company/DevelopersJobDbApp/DevelopersJobDbAppApplication.java
@@ -0,0 +1,13 @@
+package com.company.DevelopersJobDbApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopersJobDbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopersJobDbAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/DevelopersJobDbApp/DevelopersJobDbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.DevelopersJobDbApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopersJobDbAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
